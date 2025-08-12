5347767d709f41025ecac2d0f6ea1b5a61938669
Jingway <guojingwei129@gmail.com>
2025-08-12 18:09:43 +0800

    update

--- src/main/java/com/way/gtool/common/error/GlobalExceptionHandler.java
+++ src/main/java/com/way/gtool/common/error/GlobalExceptionHandler.java
@@ -3,12 +3,16 @@ package com.way.gtool.common.error;
 import com.way.gtool.common.utils.Result;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler
     public Result handlerException(Exception e) {
         System.out.println("全局异常捕获：" + e.getMessage());
+        if (e instanceof NoResourceFoundException) {
+            return Result.get(404, "资源未找到", null);
+        }
         return Result.error(e.getMessage());
     }
 }
